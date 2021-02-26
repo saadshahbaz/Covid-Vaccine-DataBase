674269b1bed2fab7a3cebe75072643f7312693c3
saadshahbaz <59991041+saadshahbaz@users.noreply.github.com>
2021-02-27 01:51:59 +0500

    Update droptbl.sql

--- src/sql_files/droptbl.sql
+++ src/sql_files/droptbl.sql
@@ -1,14 +1,3 @@
--- Include your drop table DDL statements in this file.
--- Make sure to terminate each statement with a semicolon (;)
-
--- LEAVE this statement on. It is required to connect to your database.
-CONNECT TO cs421;
-
--- Remember to put the drop table ddls for the tables with foreign key references
---    ONLY AFTER the parent tables has already been dropped (reverse of the creation order).
-
--- This is only an example of how you add drop table ddls to this file.
---   You may remove it.
 DROP TABLE Category;
 DROP TABLE Person;
 DROP TABLE Vaccine;
