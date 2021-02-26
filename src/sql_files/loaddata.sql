2c84ca4b18166389fc6f61509546c4f0a946cb0b
saadshahbaz <59991041+saadshahbaz@users.noreply.github.com>
2021-02-27 01:52:37 +0500

    Update loaddata.sql

--- src/sql_files/loaddata.sql
+++ src/sql_files/loaddata.sql
@@ -1,14 +1,3 @@
--- Include your INSERT SQL statements in this file.
--- Make sure to terminate each statement with a semicolon (;)
-
--- LEAVE this statement on. It is required to connect to your database.
-CONNECT TO cs421;
-
--- Remember to put the INSERT statements for the tables with foreign key references
---    ONLY AFTER the parent tables!
-
--- This is only an example of how you add INSERT statements to this file.
---   You may remove it.
 INSERT INTO Category VALUES('Teacher', 2);
 INSERT INTO Category VALUES('Health Care Worker', 1);
 INSERT INTO Category VALUES('Elderly', 1);
