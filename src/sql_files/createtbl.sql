8dadad4a81c210dc734ab9f15d51eed02fa06899
saadshahbaz <59991041+saadshahbaz@users.noreply.github.com>
2021-02-27 01:50:12 +0500

    Update createtbl.sql

--- src/sql_files/createtbl.sql
+++ src/sql_files/createtbl.sql
@@ -1,15 +1,3 @@
--- Include your create table DDL statements in this file.
--- Make sure to terminate each statement with a semicolon (;)
-
--- LEAVE this statement on. It is required to connect to your database.
-CONNECT TO cs421;
-
--- Remember to put the create table ddls for the tables with foreign key references
---    ONLY AFTER the parent tables has already been created.
-
--- This is only an example of how you add create table ddls to this file.
---   You may remove it.
-
 CREATE TABLE Category(
     cname VARCHAR(30) NOT NULL
     ,prioritynum INTEGER NOT NULL
